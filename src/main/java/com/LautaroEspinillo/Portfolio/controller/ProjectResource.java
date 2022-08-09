046c616443ed9dc9fe42fac48fcd1f75c49932f2
TheLaucha <espinillo.lautaro@gmail.com>
2022-08-08 23:29:31 -0300

    cross

--- src/main/java/com/LautaroEspinillo/Portfolio/controller/ProjectResource.java
+++ src/main/java/com/LautaroEspinillo/Portfolio/controller/ProjectResource.java
@@ -8,8 +8,7 @@ import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
-//@CrossOrigin
-@CrossOrigin(origins = "*")
+@CrossOrigin
 @RestController
 @RequestMapping("/projects")
 public class ProjectResource {
--- src/main/java/com/LautaroEspinillo/Portfolio/controller/SkillsController.java
+++ src/main/java/com/LautaroEspinillo/Portfolio/controller/SkillsController.java
@@ -8,8 +8,7 @@ import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
-//@CrossOrigin
-@CrossOrigin(origins = "*")
+@CrossOrigin
 @RestController
 @RequestMapping("/skills")
 public class SkillsController {
